695aa755563845ba882c8c4b3e7d0fae0bb1fdcb
feygia <feygia@elad.co.il>
2025-06-01 02:12:03 +0300

    add proj as lib

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import ChatWidget from  './components/Chat/ChatWidget'
+import { Chat } from 'ben-guryon-bot';
 
 
 function App() {
   return (
     <div className="App">
-      <ChatWidget></ChatWidget>
+      <header className="header">
+        <div className="container">
+          <div className="logo">האתר שלי</div>
+          <nav className="nav">
+            <a href="/">בית</a>
+            <a href="/about">אודות</a>
+            <a href="/contact">פולה</a>
+          </nav>
+        </div>
+      </header>
+      <Chat></Chat>
     </div>
   );
 }
---
+++ src/lib/index.js
@@ -0,0 +1,5 @@
+// src/lib/index.js
+
+export { default as Chat } from './components/Chat/ChatWidget';
+export { default as ChatMessage } from './components/ChatMessage/ChatMessage';
+export { default as CourseCard } from './components/CourseCard/CourseCard';
