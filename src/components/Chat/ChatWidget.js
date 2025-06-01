fa32fac1cd6472b6277dd8a4d7c13f191c1ecac5
feygia <feygia@elad.co.il>
2025-06-01 10:11:58 +0300

    add url back prod

--- src/components/Chat/ChatWidget.js
+++ src/components/Chat/ChatWidget.js
@@ -73,7 +73,8 @@ export default function ChatWidget() {
     try {
       setLoading(true);
       // const response = await axios.post(`https://fultgs45z1.execute-api.us-east-1.amazonaws.com/dev/process`, {
-      const response = await axios.post(`https://e1tdhkbxnh.execute-api.us-east-1.amazonaws.com/test/chat`, {
+        const response = await axios.post(`http://pola-dev.bgu.ac.il/chat`, {
+          // const response = await axios.post(`https://e1tdhkbxnh.execute-api.us-east-1.amazonaws.com/test/chat`, {----טסטים 
         // const response = await axios.post(`http://147.235.193.111:8000/chat`, {
         //  type: _type,
         message: _type != "file" ? _value : null,
